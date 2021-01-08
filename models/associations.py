b208703e70ec0142b6d5192f7f8530de97d65801
Joel Croteau <joelcroteau@joelcroteau.com>
2021-01-08 15:31:28 -0800

    Replace AssociationTable class with a function
    This makes more sense if all we want is to build a table
    with specific properties. Inheriting from `Table` is a pain
    as it does all of its initiation logic in `__new__`

--- models/associations.py
+++ models/associations.py
@@ -11,47 +11,26 @@ from .researcher import Researcher
 from .topic import Topic
 
 
-class AssociationTable(Table):
-    def __new__(cls, left: Type[DataPoint], right: Type[DataPoint], right_name: Optional[str] = None,
-                left_base_name: Optional[str] = None, left_plural: Optional[str] = None) -> 'AssociationTable':
-        if not left_base_name:
-            left_base_name = left.base_name
-        if not left_plural:
-            left_plural = left.plural
-        if not right_name:
-            right_name = right.plural
+def association_table(left: Type[DataPoint], right: Type[DataPoint], right_name: Optional[str] = None,
+                      left_base_name: Optional[str] = None, left_plural: Optional[str] = None) -> Table:
+    if not left_base_name:
+        left_base_name = left.base_name
+    if not left_plural:
+        left_plural = left.plural
+    if not right_name:
+        right_name = right.plural
 
-        left_id = Column(left.id.name, ForeignKey(left.id), primary_key=True)
-        right_id = Column(right.id.name, ForeignKey(right.id), primary_key=True)
-        new_table = super().__new__(
-            cls,
-            left_base_name + '_' + right_name,
-            Model.metadata,
-            left_id,
-            right_id
-        )
-        new_table.left_base_name = left_base_name
-        new_table.left_plural = left_plural
-        new_table.right_name = right_name
-        new_table.left_id = left_id
-        new_table.right_id = right_id
+    left_id = Column(left.id.name, ForeignKey(left.id), primary_key=True)
+    right_id = Column(right.id.name, ForeignKey(right.id), primary_key=True)
+    new_table = Table(
+        left_base_name + '_' + right_name,
+        Model.metadata,
+        left_id,
+        right_id
+    )
+    setattr(left, right_name, relationship(right, backref=left_plural, secondary=new_table))
 
-        return new_table
-
-    def __init__(self, left: Type[DataPoint], right: Type[DataPoint], *_, **__) -> None:
-        super().__init__(
-            self.name,
-            Model.metadata,
-            self.left_id,
-            self.right_id
-        )
-        setattr(left, self.right_name, relationship(right, backref=self.left_plural, secondary=self))
-
-    left_base_name: str
-    left_plural: str
-    right_name: str
-    left_id: Column
-    right_id: Column
+    return new_table
 
 
 class Affiliation(Model):
@@ -71,6 +50,6 @@ class Affiliation(Model):
 #    reading2_id = Column('reading2_id', ForeignKey(Reading.id), primary_key=True)
 
 
-reading_topics = AssociationTable(Reading, Topic)
-researcher_readings = AssociationTable(Researcher, Reading)
-institute_readings = AssociationTable(Institute, Reading)
+reading_topics = association_table(Reading, Topic)
+researcher_readings = association_table(Researcher, Reading)
+institute_readings = association_table(Institute, Reading)
